b7cadb441b4e0ba85e8ffc13a38db09a8080c241
Eric Chrobak <eric@ericchrobak.com>
2018-06-26 21:47:30 +0000

    automatically backed up by learn

--- config/environment.rb
+++ config/environment.rb
@@ -2,7 +2,10 @@ require 'bundler'
 Bundler.require
 
 require_relative '../lib/concerns/findable'
-
+require_relative '../lib/musiclibrarycontroller'
 
 
 require_all 'lib'
+library = MusicLibraryController.new
+
+library.call
